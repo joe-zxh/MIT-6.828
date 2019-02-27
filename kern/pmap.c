d1e62bc2fddd771e12b7cf87b1ac2f25df95e108
Unknown <178559145@qq.com>
2019-02-27 16:59:59 +0800

    c

--- kern/pmap.c
+++ kern/pmap.c
@@ -158,6 +158,8 @@ mem_init(void)
 	// array.  'npages' is the number of physical pages in memory.  Use memset
 	// to initialize all fields of each struct PageInfo to 0.
 	// Your code goes here:
+	pages = (struct PageInfo *) boot_alloc(npages * sizeof(struct PageInfo));
+	memset(pages, 0, npages * sizeof(struct PageInfo));
 
 
 	//////////////////////////////////////////////////////////////////////
@@ -261,12 +263,33 @@ page_init(void)
 	// Change the code to reflect this.
 	// NB: DO NOT actually touch the physical memory corresponding to
 	// free pages!
+	
 	size_t i;
-	for (i = 0; i < npages; i++) {
-		pages[i].pp_ref = 0;
-		pages[i].pp_link = page_free_list;
-		page_free_list = &pages[i];
+	page_free_list = NULL;
+
+	//num_alloc：在extmem区域已经被占用的页的个数
+	int num_alloc = ((uint32_t)boot_alloc(0) - KERNBASE) / PGSIZE;
+	//num_iohole：在io hole区域占用的页数
+	int num_iohole = 96;
+
+	for(i=0; i<npages; i++)
+	{
+		if(i==0)
+		{
+			pages[i].pp_ref = 1;
+		}    
+		else if(i >= npages_basemem && i < npages_basemem + num_iohole + num_alloc)
+		{
+			pages[i].pp_ref = 1;
+		}
+		else
+		{
+			pages[i].pp_ref = 0;
+			pages[i].pp_link = page_free_list;
+			page_free_list = &pages[i];
+		}
 	}
+
 }
 
 //
@@ -285,7 +308,21 @@ struct PageInfo *
 page_alloc(int alloc_flags)
 {
 	// Fill this function in
-	return 0;
+
+	struct PageInfo *result;
+    if (page_free_list == NULL){
+		return NULL;
+	}        
+
+    result= page_free_list;
+    page_free_list = result->pp_link; //page_free_list指向下一个空的值
+    result->pp_link = NULL;
+
+    if (alloc_flags & ALLOC_ZERO){
+		memset(page2kva(result), 0, PGSIZE); 
+	}     
+
+    return result;
 }
 
 //
@@ -298,6 +335,11 @@ page_free(struct PageInfo *pp)
 	// Fill this function in
 	// Hint: You may want to panic if pp->pp_ref is nonzero or
 	// pp->pp_link is not NULL.
+	assert(pp->pp_ref == 0);
+    assert(pp->pp_link == NULL);
+
+    pp->pp_link = page_free_list;
+    page_free_list = pp;
 }
 
 //
@@ -460,6 +502,7 @@ check_page_free_list(bool only_low_memory)
 		panic("'page_free_list' is a null pointer!");
 
 	if (only_low_memory) {
+		//相当于page_free_list指向地址小的位置开始。
 		// Move pages with lower addresses first in the free
 		// list, since entry_pgdir does not map all pages.
 		struct PageInfo *pp1, *pp2;
