f93b4f82ef73cb84aa10e1b5292f8e2092354f8c
Unknown <178559145@qq.com>
2019-03-04 09:23:07 +0800

    Exercise 7

--- kern/trap.c
+++ kern/trap.c
@@ -186,14 +186,35 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
 
-	// Unexpected trap: The user process or the kernel has a bug.
-	print_trapframe(tf);
-	if (tf->tf_cs == GD_KT)
-		panic("unhandled trap in kernel");
-	else {
-		env_destroy(curenv);
-		return;
-	}
+	int32_t ret_code;
+	 switch(tf->tf_trapno) {
+        case (T_PGFLT):
+            page_fault_handler(tf);
+            break;
+		case (T_BRKPT):
+            monitor(tf);
+			break; 
+		case (T_SYSCALL):
+			ret_code = syscall(
+					tf->tf_regs.reg_eax,
+					tf->tf_regs.reg_edx,
+					tf->tf_regs.reg_ecx,
+					tf->tf_regs.reg_ebx,
+					tf->tf_regs.reg_edi,
+					tf->tf_regs.reg_esi);
+			tf->tf_regs.reg_eax = ret_code;
+			break;
+
+        default:
+            // Unexpected trap: The user process or the kernel has a bug.
+            print_trapframe(tf);
+            if (tf->tf_cs == GD_KT)
+                panic("unhandled trap in kernel");
+            else {
+                env_destroy(curenv);
+                return;
+            }
+    }
 }
 
 void
