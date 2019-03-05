b307e753d087ac7615797478d104a5625b805070
Unknown <178559145@qq.com>
2019-03-05 20:05:29 +0800

    Exercize 7 bug fixed

--- kern/init.c
+++ kern/init.c
@@ -55,10 +55,13 @@ i386_init(void)
 #else
 	// Touch all you want.
 	// ENV_CREATE(user_primes, ENV_TYPE_USER);
+
+    // ENV_CREATE(user_yield, ENV_TYPE_USER);
+    // ENV_CREATE(user_yield, ENV_TYPE_USER);
+    // ENV_CREATE(user_yield, ENV_TYPE_USER);
+
+	ENV_CREATE(user_dumbfork, ENV_TYPE_USER);
 #endif // TEST*
-    ENV_CREATE(user_yield, ENV_TYPE_USER);
-    ENV_CREATE(user_yield, ENV_TYPE_USER);
-    ENV_CREATE(user_yield, ENV_TYPE_USER);
 
 
 	// Schedule and run the first user environment!
